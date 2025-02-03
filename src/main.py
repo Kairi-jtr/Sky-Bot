b545ad090ec47bd360cd65fc6a78bcc8dc2277ed
Kairi-jtr <Ka24f1c@tutamail.com>
2025-02-03 21:58:31 +0900

    test

--- src/main.py
+++ src/main.py
@@ -1,9 +1,8 @@
 import discord
 from discord.ext import commands, tasks
 from discord import app_commands
-import asyncio
-import re
 import os
+from datetime import timedelta
 
 intents = discord.Intents.default()
 intents.message_content = True 
@@ -11,10 +10,6 @@ intents.message_content = True
 bot = commands.Bot(command_prefix='/', intents=intents)
 
 message_authors = []
-
-@bot.tree.command(name="hello",description="テストコマンドです。")
-async def test_command(interaction: discord.Interaction):
-    await interaction.response.send_message("てすと！",ephemeral=True)
 
 @bot.event
 async def on_ready():
@@ -24,15 +19,27 @@ async def on_ready():
 
     await bot.tree.sync()
 
+@bot.tree.command(name="rules",description="鯖の制限を決めるbot")
+async def decide_rules(interaction: discord.Interaction, text:str):
+    channel = discord.utils.get(interaction.guild.channels, name='server-rules')
+    if channel:
+        print('t')
+    else:
+        await interaction.response.send_message('server_rulesが見つかりませんでした。チャンネルを作成してください。')
+
 @bot.event
 async def on_message(message):
     if message.author.bot:
         return
 
     #メッセージ保存
-    text = message.content
     author = message.author
     author_name = author.name
+
+    if message.content in 'discord.gg':
+        timeout_duration = timedelta(minutes=5)
+        await message.author.timeout(timeout_duration)
+        message.delete()  
 
     a = False
 
@@ -49,17 +56,18 @@ async def on_message(message):
 
     await bot.process_commands(message)
 
-@tasks.loop(seconds=5)
+@tasks.loop(seconds=3)
 async def arashi_check():
-    text = []
     for i in range(len(message_authors)):
         msg_list = message_authors[i][1]
-        if len(msg_list) > 5:
-            for k in msg_list:
-                await k.delete()
+        if len(msg_list) > 3:
+            #timeout_duration = timedelta(minutes=5)
+            #await msg_list[0].author.timeout(timeout_duration)
 
-            channnel = bot.get_channel(1332697911502438412)
-            await channnel.send("スパムを削除しました。")
+            for msg in msg_list:
+                await msg.delete()            
+
+            print('スパムを削除')
 
     message_authors.clear()
     print('reset')
